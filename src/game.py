15d6719be5426891c8d9d1177830e312020b37e3
James Ekern <jamesekern@Jamess-MacBook-Air.local>
2026-05-18 13:14:53 -0500

    updated game_state to track previous rolls. TODO: make payout formula

--- src/game.py
+++ src/game.py
@@ -15,14 +15,15 @@ class Game:
 
     def peek(self):
         roll = self.die1.roll() + self.die2.roll()
+        self.state.past_rolls.append(roll)
         return roll
     
     def calc_payout(self, guess:int, roll:int) -> float:
         return 0 #insert payout formula here
     
     def guess(self, guess:int):
-
         roll = self.die1.roll() + self.die2.roll()
         payout = self.calc_payout(guess, roll)
         self.state.net_score += payout
+        self.state.past_rolls.append(roll)
         return payout
--- src/game_state.py
+++ src/game_state.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, List
 from depleting_dice import DepletingDice
 
 
@@ -9,4 +9,5 @@ class GameState:
     current_dice: Tuple[DepletingDice, DepletingDice]
     net_score: float = 0
     current_round: int = 0
+    past_rolls: List[int] = []
 
