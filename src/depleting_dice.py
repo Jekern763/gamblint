99b813b40728147f6de357d5819d7dc6d7657b5c
James Ekern <jamesekern@Jamess-MacBook-Air.local>
2026-05-18 11:05:40 -0500

    completed DepletingDice class with reset and roll methods

---
+++ src/depleting_dice.py
@@ -0,0 +1,16 @@
+from random import choice
+class DepletingDice:
+    def __init__(self, num_sides:int=6):
+        self.num_sides = num_sides
+        self.current_sides = list(range(1, num_sides))
+
+    def __len__(self):
+        return len(self.current_sides)
+    
+    def reset(self):
+        self.current_sides = list(range(1, self.num_sides))
+
+    def roll(self, remove:bool=True) -> int:
+        roll =  choice(self.current_sides)
+        self.current_sides.remove(roll)
+        return roll
