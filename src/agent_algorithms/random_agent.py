096caf4991d2b858d69140a6053c9d0ef7a74120
James Ekern <jamesekern@Jamess-MacBook-Air.local>
2026-05-20 16:43:17 -0500

    created a pseudo random agent as a control for all agent algorithms

---
+++ src/agent_algorithms/random_agent.py
@@ -0,0 +1,7 @@
+from agent import Agent
+import random
+
+# this algorithm randomly guesses a number between 2 and 12, ignoring the peeks. This is a baseline for testing the game engine and the agent framework. It should perform worse than any algorithm that uses the peeks, but it should still be able to win some rounds by pure luck.
+class RandomAgent(Agent):
+    def get_action(self, peeks):
+        return random.randint(2, 12)
